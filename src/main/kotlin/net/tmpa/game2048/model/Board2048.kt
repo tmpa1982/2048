7ddfb6887da21c920dc1f5ae55cc981a8b806a33
Tran Minh Phuong Anh <tran.minh.phuong.anh@gmail.com>
2025-09-09 23:39:17 +0800

    Ensure there are at least two V2 cells

--- src/main/kotlin/net/tmpa/game2048/model/Board2048.kt
+++ src/main/kotlin/net/tmpa/game2048/model/Board2048.kt
@@ -8,12 +8,33 @@ class Board2048 {
 
         fun initializeRandomBoard(valueGenerator: () -> CellValue = { allowedInitialValues.random() }) : Board2048 {
             val board = Board2048()
+            var nonEmptyCount = 0
             for (r in 0 until SIZE) {
                 for (c in 0 until SIZE) {
-                    board.setCellValue(r, c, valueGenerator())
+                    val value = valueGenerator()
+                    board.setCellValue(r, c, value)
+                    if (value != CellValue.EMPTY) {
+                        nonEmptyCount++
+                    }
                 }
             }
+
+            if (nonEmptyCount < 2) {
+                ensureTwoNonEmptyCells(board)
+            }
             return board
+        }
+
+        private fun ensureTwoNonEmptyCells(board: Board2048) {
+            var placed = 0
+            while (placed < 2) {
+                val r = (0 until SIZE).random()
+                val c = (0 until SIZE).random()
+                if (board.getCellValue(r, c) == CellValue.EMPTY) {
+                    board.setCellValue(r, c, CellValue.V2)
+                    placed++
+                }
+            }
         }
     }
 
--- src/test/kotlin/net/tmpa/game2048/model/Board2048Test.kt
+++ src/test/kotlin/net/tmpa/game2048/model/Board2048Test.kt
@@ -18,5 +18,19 @@ class Board2048Test {
                 }
             }
         }
+
+        @Test
+        fun `initialized board has at least two V2 cells`() {
+            val board = Board2048.initializeRandomBoard { CellValue.EMPTY }
+            var v2Count = 0
+            for (r in 0 until Board2048.SIZE) {
+                for (c in 0 until Board2048.SIZE) {
+                    if (board.getCellValue(r, c) == CellValue.V2) {
+                        v2Count++
+                    }
+                }
+            }
+            assertTrue(v2Count >= 2, "Board should have at least two cells with value 2")
+        }
     }
 }
