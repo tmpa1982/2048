f4cf2b23a3092ee433842e69ea826c77424d3aec
Tran Minh Phuong Anh <tran.minh.phuong.anh@gmail.com>
2025-09-11 20:56:36 +0800

    Work around langchain4j shortcomings in handling generics

--- src/main/kotlin/net/tmpa/game2048/ai/GameTools.kt
+++ src/main/kotlin/net/tmpa/game2048/ai/GameTools.kt
@@ -5,45 +5,59 @@ import net.tmpa.game2048.model.Board2048
 import net.tmpa.game2048.model.CellValue
 import org.slf4j.LoggerFactory
 
+data class BoardDto(
+    val list: List<CellValue>,
+    val size: Int,
+)
+
 class GameTools {
     private val logger = LoggerFactory.getLogger(GameTools::class.java)
 
     @Tool("Move the 2048 game board to the left")
-    fun mergeLeft(board: List<List<CellValue>>): List<List<CellValue>> {
+    fun mergeLeft(board: BoardDto): BoardDto {
         logger.info("Tools call - merging left for board: $board")
         return merge(board) { it.mergeLeft() }
     }
 
     @Tool("Move the 2048 game board to the right")
-    fun mergeRight(board: List<List<CellValue>>): List<List<CellValue>> {
+    fun mergeRight(board: BoardDto): BoardDto {
         logger.info("Tools call - merging right for board: $board")
         return merge(board) { it.mergeRight() }
     }
 
     @Tool("Move the 2048 game board up")
-    fun mergeUp(board: List<List<CellValue>>): List<List<CellValue>> {
+    fun mergeUp(board: BoardDto): BoardDto {
         logger.info("Tools call - merging up for board: $board")
         return merge(board) { it.mergeUp() }
     }
 
     @Tool("Move the 2048 game board down")
-    fun mergeDown(board: List<List<CellValue>>): List<List<CellValue>> {
+    fun mergeDown(board: BoardDto): BoardDto {
         logger.info("Tools call - merging down for board: $board")
         return merge(board) { it.mergeDown() }
     }
 
     @Tool("Evaluate if the 2048 game board is winning")
-    fun isWinning(board: List<List<CellValue>>): Boolean {
+    fun isWinning(board: BoardDto): Boolean {
         logger.info("Tools call - evaluating if board is winning: $board")
-        return Board2048(board).isWinning()
+        return createTwoDimensionalBoard(board).isWinning()
     }
 
     @Tool("Evaluate if the 2048 game board is losing")
-    fun isLosing(board: List<List<CellValue>>): Boolean {
+    fun isLosing(board: BoardDto): Boolean {
         logger.info("Tools call - evaluating if board is losing: $board")
-        return Board2048(board).isLosing()
+        return createTwoDimensionalBoard(board).isLosing()
     }
 
-    private fun merge(board: List<List<CellValue>>, transformer: (Board2048) -> Board2048) =
-        transformer(Board2048(board)).asList()
+    private fun merge(board: BoardDto, transformer: (Board2048) -> Board2048) =
+        BoardDto(transformer(createTwoDimensionalBoard(board)).asList().flatten(), board.size)
+
+    private fun createTwoDimensionalBoard(board: BoardDto): Board2048 {
+        val size = board.size
+        val twoDimensionalBoard = Array(size) { Array(size) { CellValue.EMPTY } }
+        for (i in board.list.indices) {
+            twoDimensionalBoard[i / size][i % size] = board.list[i]
+        }
+        return Board2048(twoDimensionalBoard)
+    }
 }
