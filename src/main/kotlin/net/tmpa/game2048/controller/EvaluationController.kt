e811c5606117a55f0e4315671bb10143247d02ea
Tran Minh Phuong Anh <tran.minh.phuong.anh@gmail.com>
2025-09-11 22:56:18 +0800

    Reuse list based board constructor

--- src/main/kotlin/net/tmpa/game2048/controller/EvaluationController.kt
+++ src/main/kotlin/net/tmpa/game2048/controller/EvaluationController.kt
@@ -4,7 +4,6 @@ import net.tmpa.game2048.ai.AiEvaluator
 import net.tmpa.game2048.dto.EvaluationRequest
 import net.tmpa.game2048.dto.EvaluationResponse
 import net.tmpa.game2048.model.Board2048
-import net.tmpa.game2048.model.CellValue
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -19,7 +18,7 @@ class EvaluationController(private val evaluator: AiEvaluator) {
     @PostMapping
     fun evaluateBoard(@RequestBody request: EvaluationRequest): EvaluationResponse {
         logger.info("Evaluating board: ${request.board}")
-        val board = Board2048(request.board.map { it.toTypedArray<CellValue>() }.toTypedArray<Array<CellValue>>())
+        val board = Board2048(request.board)
         val moveEvaluation = evaluator.evaluate(board)
 
         val response = EvaluationResponse(moveEvaluation, board.nextBoard(moveEvaluation.bestMove).asList())
