ab3c3b86ad02c7e4c9408b0800c9e8861e370b42
Tran Minh Phuong Anh <tran.minh.phuong.anh@gmail.com>
2025-09-12 23:15:31 +0800

    CORS

--- src/main/kotlin/net/tmpa/game2048/Application.kt
+++ src/main/kotlin/net/tmpa/game2048/Application.kt
@@ -2,9 +2,22 @@ package net.tmpa.game2048
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
-class Application
+class Application {
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer = object : WebMvcConfigurer {
+        override fun addCorsMappings(registry: CorsRegistry) {
+            registry
+                .addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+        }
+    }
+}
 
 fun main(args: Array<String>) {
 	runApplication<Application>(*args)
