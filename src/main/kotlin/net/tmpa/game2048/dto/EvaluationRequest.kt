09fa3d0217ceec5e9b00aac96fe42baeaccadc73
Tran Minh Phuong Anh <tran.minh.phuong.anh@gmail.com>
2025-09-13 00:26:34 +0800

    Do not show isValid in swagger

--- src/main/kotlin/net/tmpa/game2048/dto/EvaluationRequest.kt
+++ src/main/kotlin/net/tmpa/game2048/dto/EvaluationRequest.kt
@@ -1,8 +1,10 @@
 package net.tmpa.game2048.dto
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import net.tmpa.game2048.model.CellValue
 
 class EvaluationRequest(val board: List<List<CellValue>>) {
+    @JsonIgnore
     fun isValid(): Boolean {
         val size = board.size
         if (size == 0) return false
