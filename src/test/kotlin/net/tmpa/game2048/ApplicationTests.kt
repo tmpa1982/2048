1de3102956747c2cec06b1682f1fef795614fd40
Tran Minh Phuong Anh <tran.minh.phuong.anh@gmail.com>
2025-09-13 12:41:53 +0800

    Revert "Fail test to verify pipeline"
    This reverts commit 501d9aedd79ccf888f53313451fc4e576e3e5d55.

--- src/test/kotlin/net/tmpa/game2048/ApplicationTests.kt
+++ src/test/kotlin/net/tmpa/game2048/ApplicationTests.kt
@@ -2,14 +2,12 @@ package net.tmpa.game2048
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-import kotlin.test.fail
 
 @SpringBootTest
 class ApplicationTests {
 
 	@Test
 	fun contextLoads() {
-        fail()
 	}
 
 }
