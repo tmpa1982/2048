4c237edb304016f03d59d17d857719f6bd87a190
Tran Minh Phuong Anh <tran.minh.phuong.anh@gmail.com>
2025-09-14 11:07:47 +0800

    Add test for cell value enum logic

---
+++ src/test/kotlin/net/tmpa/game2048/model/CellValueTest.kt
@@ -0,0 +1,26 @@
+package net.tmpa.game2048.model
+
+import org.junit.jupiter.api.assertThrows
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class CellValueTest {
+    @Test
+    fun `test enum values`() {
+        var expectedCellValue = CellValue.EMPTY
+        var expectedNumeric = 0
+        for (cellValue in CellValue.entries) {
+            assertEquals(expectedCellValue, cellValue)
+            assertEquals(expectedNumeric, cellValue.value)
+            if (cellValue != CellValue.V2048) {
+                expectedCellValue = cellValue.getNext()
+                expectedNumeric = if (expectedNumeric == 0) 2 else expectedNumeric * 2
+            }
+        }
+    }
+
+    @Test
+    fun `next value on 2048 throws exception`() {
+        assertThrows<IllegalStateException> { CellValue.V2048.getNext() }
+    }
+}
